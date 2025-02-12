9f22417c8e7a4d36e5268eea93ffd6b764a12644
Titusz87 <titus.tarcsai@gmail.com>
2025-02-12 18:00:45 +1030

    Add files via upload

---
+++ AStar_MazeProject/AStar_MazeProject/NodeExplorer.cs
@@ -0,0 +1,138 @@
+﻿namespace AStar_MazeProject
+{
+     class NodeExplorer
+    {
+        // Calculates the Manhattan distance between the current point and the start point.
+        public static int EdgeCostMethod(int[] currentPoint, int[] startPoint)
+        {
+            return Math.Abs((currentPoint[0] - startPoint[0]))
+                                   + Math.Abs((currentPoint[1] - startPoint[1]));
+        }
+
+        // Calculates the squared Euclidean distance (hypotenuse squared) between the current point and the goal.
+        public static int HeuristicCostMethod(int[] currentPoint, int[] goal)
+        {
+            int a_Leg = Math.Abs(currentPoint[0] - goal[0]);
+            int b_Leg = Math.Abs(currentPoint[1] - goal[1]);
+            double hypotenuse = Math.Pow(a_Leg, 2) + Math.Pow(b_Leg, 2);
+
+            return (int)hypotenuse;
+        }
+
+        // Combines the heuristic cost and edge cost to calculate the total cost.
+        public static int TotalCostMethod(int heuristicCost, int edgeCost)
+        {
+            return heuristicCost + edgeCost;
+        }
+
+        // Iterates through the open list of nodes to find the node with the lowest total cost.
+        public static int FindBestCost()
+        {
+            int bestCost = 500;
+
+            foreach (Node node in PathFinderState.OpenListQueue)
+            {
+                int totalCost = node.TotalCost;
+                if (totalCost < bestCost)
+                {
+                    bestCost = totalCost;
+                }
+            }
+            return bestCost;
+        }
+
+        // Retrieves the node with the best (lowest) total cost from the open list.
+        // This node is then added to the path list for further processing.
+        public static Node GetNodeWithBestCost(int bestCost)
+        {
+            PathFinderState.ListOpenList = PathFinderState.OpenListQueue.ToList();
+
+            Node bestNode = PathFinderState.ListOpenList.Find(node => node.TotalCost == bestCost);
+            PathFinderState.PathList.Add(bestNode);
+            return bestNode;
+        }
+
+        // Removes a node from the open list once it has been explored.
+        public static void RemoveNodeFromOpenList(Node bestNode)
+        {
+            int index = PathFinderState.ListOpenList.IndexOf(bestNode);
+            PathFinderState.ListOpenList.RemoveAt(index);
+        }
+
+        // Checks if a node has already been explored by comparing its coordinates with those in the explored queue.
+        public static bool CheckIfNodeExplored(Node obj, int[] coordChanges)
+        {
+
+            foreach (Node exploredNode in PathFinderState.ExploredQueue.ToList())
+            {
+                int checkForNodeRow = obj.Row + coordChanges[0];
+                int checkForNodeColumn = obj.Column + coordChanges[1];
+
+                if (exploredNode.Row == checkForNodeRow
+                 && exploredNode.Column == checkForNodeColumn)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Explores a node by calculating its position and adding it to the explored queue.
+        public static void ExploreNode(Node currentNode, Node startNode,
+                                       Node goalNode, int[] coordChanges)
+        {
+            int[] currentNodePosition = [currentNode.Row, currentNode.Column];
+            int[] startNodePosition = [startNode.Row, startNode.Column];
+            int[] goalNodePosition = [goalNode.Row, goalNode.Column];
+
+            Child child = new Child(currentNodePosition, startNodePosition, goalNodePosition, coordChanges, currentNodePosition);
+            PathFinderState.ExploredQueue.Enqueue(child);
+
+            if (child.State != (int)Maze.MazeComponents.Wall)
+            {
+                PathFinderState.OpenListQueue.Enqueue(child);
+            }
+        }
+
+        // Iterates through possible directions (down, right, up, left) to find valid neighboring nodes.
+        // If a neighboring node has not been explored, it is added to the open list.
+        public static void GetValidNeighbourVisited(Node current, Node start, Node goal)
+        {
+            List<int[]> directions = new List<int[]>();
+            directions.Add([-1, 0]);    // going down 
+            directions.Add([ 0, 1]);    // going right
+            directions.Add([ 1, 0]);    // going up
+            directions.Add([ 0,-1]);    // going left
+
+            foreach (int[] direction in directions)
+            {
+                bool result = NodeExplorer.CheckIfNodeExplored(current, direction);
+
+                if (!result)
+                {
+                    NodeExplorer.ExploreNode(current, start, goal, direction);
+                }
+            }
+        }
+
+        // Recursively backtracks from the current node to the start node to reconstruct the shortest path.
+        public static void Backtracking(Node backtrackingFrom, Node backtrackingTo)
+        {
+            PathFinderState.ShortestPathList.Add(backtrackingFrom);
+            int[] currentParrent = [backtrackingFrom.Parent[0], backtrackingFrom.Parent[1]];
+
+            foreach (Node neighbour in PathFinderState.PathList)
+            {
+                if (currentParrent[0] == backtrackingTo.Row && currentParrent[1] == backtrackingTo.Column)
+                {
+                    break;
+                }
+                if (neighbour.Row == currentParrent[0] && neighbour.Column == currentParrent[1])
+                {
+                    PathFinderState.ShortestPathList.Add(neighbour);
+                    Backtracking(neighbour, backtrackingTo);
+                }
+            }
+        }
+    }
+}
---
+++ AStar_MazeProject/AStar_MazeProject/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace AStar_MazeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isFirstgoalPassed = false;
+            bool isEndGoalFound = false;
+            Node startNode = MazeState.InitState;
+            Node goalNode = MazeState.FirstGoal;
+
+            //Creates and displays the maze environment
+            Node[,] maze = Maze.CreateMaze([startNode.Row, startNode.Column], [goalNode.Row, goalNode.Column]);
+            Display.simulationPartOne(maze);
+
+            // A* search logic ↓
+            // Marks starting position as explored        
+            PathFinderState.ExploredQueue.Enqueue(startNode);
+            PathFinderState.OpenListQueue.Enqueue(startNode);
+
+            while (PathFinderState.OpenListQueue.Count > 0)
+            {
+                // Searches for lowest Total cost
+                int bestCost = NodeExplorer.FindBestCost();
+                Node bestNode = NodeExplorer.GetNodeWithBestCost(bestCost);
+                NodeExplorer.RemoveNodeFromOpenList(bestNode);
+                PathFinderState.OpenListQueue = new Queue<Node>(PathFinderState.ListOpenList);
+
+                // Checks if the First goal is found. In case it is found, it sets the new goal
+                // and computes costs from that position to the end goal.
+                Node currentNode = bestNode;
+                if (!isFirstgoalPassed)
+                {
+                    if (currentNode.Row == goalNode.Row
+                     && currentNode.Column == goalNode.Column)
+                    {
+                        startNode = MazeState.FirstGoal;
+                        goalNode = MazeState.EndGoal;
+                        maze = Maze.CreateMaze([startNode.Row, startNode.Column], [goalNode.Row, goalNode.Column]);
+
+                        isFirstgoalPassed = true;
+                    }
+                }
+                // Checks if the End goal is found
+                if (currentNode.Row == goalNode.Row
+                 && currentNode.Column == goalNode.Column)
+                {
+                    Display.simulationPartTwo(maze);
+                    isEndGoalFound = true;
+                    break;
+                }
+                else
+                {
+                    // Searches for neighbours inside the maze's boundaries
+                    if (currentNode.Column <= Maze.mazeLastColumn
+                     && currentNode.Column > Maze.mazeFirstColumn
+                     && currentNode.Row <= Maze.mazeLastRow
+                     && currentNode.Row > Maze.mazeFirstRow)
+                    {
+                        NodeExplorer.GetValidNeighbourVisited(currentNode, startNode, goalNode);
+                    }
+                }
+            }
+            if (isEndGoalFound)
+            {
+                //Displays the shortest path to reach the goal
+                PathFinderState.PathList.Reverse();
+                NodeExplorer.Backtracking(MazeState.EndGoal, MazeState.InitState);
+                Maze.UpdateMaze(maze, PathFinderState.ShortestPathList, (int)Maze.MazeComponents.ShortestPath);
+                Display.DisplayMaze(maze);
+                Display.closeProgram(maze);
+            }
+            else
+            {
+                Console.WriteLine("There is no solution for this problem.");
+                return;
+            }
+        }
+    }
+ }
